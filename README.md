c471d63639bd1483b2dc743d1b3b5e34cdae39cf
iMAC 2015 <38024526+NitramDivad@users.noreply.github.com>
2018-07-29 22:50:24 -0600

    Sunday full function push

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# liri-node-app
-LIRI Node App
+# ** Node.JS Assigning:  liri-node-app
+#
+###LIRI Node App
+
+
+LIRI is a _Language_ Interpretation and Recognition Interface. LIRI is a command line node app that accepts parameters and returns data based on that input.
+
+LIRI will accept input to retrieve data from the Twitter, Spotify and OMDB API's.  
+
+Input string format for the LIRI commands are as follows:
+
+    * 'my-tweets'
+        * The 20 most recent tweets will be returned for the Twitter handle
+        * Following this command should be the Twitter handle
+        * Example:  node liri.js 'my-tweets' 'nasa'
+
+    * 'spotify-this-song'
+        * The top 5 results will be returned for the song title
+        * Following this command should be the title of the song
+        * Example:  node liri.js 'spotify-this-song' 'Lyin Eyes'
+        * Note:  If a song title is not entered, a default title of 'The Sign' by Ace of Base will be used
+
+    * 'movie-this'
+        * Following this command should be the title of the movie
+        * Example:  node liri.js 'movie-this' 'Star Wars'
+        * Note:  If a movie title is not entered, a default title of 'Mr. Nobody' will be used
+
+    * 'do-what-it-says'
+        * This command is a stand-alone command
+        * It retrieves the command along with its parameter from the random.txt file
+        * Currently it is set to perform a 'spotify-this-song' for 'I Want it That Way'
+
+As well as logging the output to the console, the output is written to a log file named log.txt.
+
+
+## **Created by:** #
+
+[David Martin](mailto:webdevelopment.du@gmail.com)
+[GitHub](https://github.com/nitramdivad)
+
+[Launch Site](https://nitramdivad.github.io/)
+
--- liri.js
+++ liri.js
@@ -7,24 +7,27 @@ var request = require('request'),
     Spotify = require('node-spotify-api'),
 
     argvs = process.argv,
+    doWIS = false,
+    results = '',
+    whatSearch = '';
     
     spotify = new Spotify(keys.spotify),
     twitter = new Twitter(keys.twitter);
 
 
-initialize();
+initializeLiri();
 
 //***************************************************************/
-function initialize() {
+function initializeLiri() {
 //***************************************************************/
 
-    var whichCommand = '',
-        whatSearch = '';
+    var whichCommand;
 
     whichCommand = argvs[2];
-    whatSearch = buildSearch(argvs, 3)
+    whatSearch = buildSearch(argvs, 3);
+
     liri(whichCommand, whatSearch);
-}
+};
 
 //***************************************************************/
 function liri(command, searchTerm) {
@@ -44,36 +47,47 @@ function liri(command, searchTerm) {
             callDWIS();
             break;
         default:
-            console.log('Function Not Defined');
+            consoleAndLog('\nFunction Not Defined','Command Not Found');
     }
-}
+};
 
 //***************************************************************/
 function buildSearch(items, idx) {
 //***************************************************************/
 
     var strSearch;
+
     for (var i = idx; i < items.length; i++) {
         if (i > idx)
             strSearch += '+' + items[i];
         else
             strSearch = items[i];
     }
-    console.log(strSearch)
+
     return strSearch;
-}
+};
 
 //***************************************************************/
 function callTwitter(handle) {
 //***************************************************************/
 
     var params = {q: 'from:'+handle, result_type: 'recent', count: 20};
+
+    results = '';
     twitter.get('search/tweets', params, function(error, tweets, response) {
         if (!error) {
-            (tweets.statuses).forEach(function(tweet) {
-                console.log('\n' + tweet.text + '\n' + tweet.created_at + '\n')
-            });
+            if ((tweets.statuses).length > 0) {
+                (tweets.statuses).forEach(function(tweet) {
+                    results += '\n' + tweet.text + '\n' + tweet.created_at + '\n';
+                });
+                consoleAndLog(results, 'Twitter Call');
+            }
+            else {
+                consoleAndLog('\nNo tweets for Twitter handle or no handle provided for call', 'Twitter Call')
+            }
         }
+        else
+            consoleAndLog(error, 'Twitter Call');
     });
 };
 
@@ -82,72 +96,110 @@ function callSpotify(title) {
 //***************************************************************/
 
     var params = {type: 'track',
-                    query: title != undefined ? title : 'The+Sign+Ace+Of+Base',
-                    limit: 1}
+                  query: title != undefined ? title : 'The+Sign+Ace+Of+Base',
+                  limit: 5};
 
+    results = '';
     spotify.search(params, function(error, data) {
         if (!error) {
             if ((data.tracks.items).length > 0) {
                 (data.tracks.items).forEach(function(song) {
-                    console.log('\nAlbum: ' + song.album.name +
-                                '\nSong: ' + song.name +
-                                '\nArtist: ' + song.artists[0].name +
-                                '\nPreview: ' + (song.preview_url !== null ? song.preview_url : 'Not available')
-                                )
-                })
+                    results += '\nAlbum: ' + song.album.name +
+                               '\nSong: ' + song.name +
+                               '\nArtist: ' + song.artists[0].name +
+                               '\nPreview: ' + (song.preview_url !== null ? song.preview_url : 'Not available') + '\n'
+                });
+                consoleAndLog(results, 'Spotify Call');
             }
             else
-                console.log('\nSong: ' + title + ' was not found.')
+                consoleAndLog('\nSong: ' + title + ' was not found.', 'Spotify Call');
         }            
     });
-}
+};
 
 //***************************************************************/
 function callOMDB(movieName) {
 //***************************************************************/
 
     var title = movieName != undefined ? movieName : 'Mr.+Nobody',
+
     params = 'http://www.omdbapi.com/?t=' + 
              title +
              '&type=movie' +
              '&tomatoes=true' +
              '&apikey=trilogy'
 
+    results = '';
     request(params, function(error, response, data) {
 
         if (!error && response.statusCode === 200) {
             var movie = JSON.parse(data);
-            var tomato = movie.tomatoRating;
 
-            (movie.Ratings).forEach(function(rating) {
-                if (rating.Source === 'Rotten Tomatoes')
-                    tomato = rating.Value;
-            });
+            if (movie.Error === 'Movie not found!')
+                consoleAndLog('\n' + movie.Error, 'OMDB Call');
+            else if (movie.Response === 'True') {
+                var tomato = movie.tomatoRating;
+                
+                if ((movie.Ratings).length > 0) {
+                    (movie.Ratings).forEach(function(rating) {
+                        if (rating.Source === 'Rotten Tomatoes')
+                            tomato = rating.Value;
+                    });
+                }
 
-            console.log('\nTitle: ' + movie.Title +
-                        '\nYear Released: ' + movie.Year +
-                        '\nIMDB Rating: ' + movie.imdbRating +
-                        '\nRotten Tomatoes Rating: ' + tomato +
-                        '\nProduction Country: ' + movie.Country +
-                        '\nMovie Language: ' + movie.Language +
-                        '\nMovie Plot: ' + movie.Plot +
-                        '\nActors : ' + movie.Actors
-                    )
+                results = '\nTitle: ' + movie.Title +
+                          '\nYear Released: ' + movie.Year +
+                          '\nIMDB Rating: ' + movie.imdbRating +
+                          '\nRotten Tomatoes Rating: ' + tomato +
+                          '\nProduction Country: ' + movie.Country +
+                          '\nMovie Language: ' + movie.Language +
+                          '\nMovie Plot: ' + movie.Plot +
+                          '\nActors: ' + movie.Actors;
+                
+                consoleAndLog(results, 'OMDB Call');
+            }
+            else
+                consoleAndLog('\nUnknown Error', 'OMDB Call');
         }
+        else
+            consoleAndLog(error, 'OMDB Call');
     });
-}
+};
 
 //***************************************************************/
 function callDWIS() {
 //***************************************************************/
 
+    results = '';
     fs.readFile('random.txt', 'utf8', function(error, data) {
         if (!error) {
             var dwis = data.split(',');
+
+            doWIS = true;
             whatSearch = buildSearch(dwis[1].split(' '), 0);
             liri(dwis[0], whatSearch);
         }
         else
-            console.log(error);
+            consoleAndLog(error, 'Do What It Says Call');
     });
-}
+};
+
+//***************************************************************/
+function consoleAndLog(results, whichCall) {
+//***************************************************************/
+
+    var header = '\n\n\n//******************************************/' +
+                 '\n' + whichCall + 
+                 (doWIS === true ? '\n  via Do What It Says' : '') +
+                 '\n' + Date() +
+                 '\n//******************************************/\n'
+
+    fs.appendFile("log.txt", header + results, function(error) {
+        if (error) {
+            console.log(error);
+        };
+    });
+
+    console.log(header + results + '\n');
+    doWIS = false;
+};
